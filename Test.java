0ab42f6cf31bf123becac66f4e54ba2280a03aff
Mihai Mancas <Mihai.Mancas@endava.com>
2019-07-30 17:56:30 +0300

    Test.java web server

---
+++ Test.java
@@ -0,0 +1,113 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Postgres {
+    public String date = new SimpleDateFormat("dd-MM-yyyy-HH-mm").format(new Date());
+    private final String createTable = "CREATE TABLE \"" + date + "\" (id INT, data VARCHAR(100));";
+    private static final int LoginTimeout = 10;
+
+    public Postgres() {
+    }
+
+    public Connection createConnection() throws IOException, ClassNotFoundException, SQLException {
+        
+        String host;
+        String username;
+        String password;
+        String driver;
+
+        host = "jdbc:postgresql://localhost:5432/docker";
+        username = "docker";
+        password = "docker";
+        driver = "org.postgresql.Driver";
+
+        System.out.println(
+                "host: " + host + "\nusername: " + username + "\npassword: " + password + "\ndriver: " + driver);
+        Class.forName(driver);
+        System.out.println("--------------------------");
+        System.out.println("DRIVER: " + driver);
+        System.out.println("Set Login Timeout: " + LoginTimeout);
+        DriverManager.setLoginTimeout(LoginTimeout);
+        Connection connection = DriverManager.getConnection(host, username, password);
+        System.out.println("CONNECTION: " + connection);
+        return connection;
+    }
+
+    public String runSqlStatement() {
+        String result = "";
+        try {
+            Statement statement = createConnection().createStatement();
+            System.out.println("SQL query: " + createTable);
+            statement.execute(createTable);
+        } catch (IOException | ClassNotFoundException ex) {
+            result = ex.getMessage();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            result = ex.getMessage();
+        }
+        return result;
+    }
+
+}
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
+        server.createContext("/test", new MyHandler());
+        server.createContext("/test2", new MyHandler2());
+        server.createContext("/postgres", new HandlerPostgres());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class HandlerPostgres implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = new Postgres().runSqlStatement();
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class MyHandler2 implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is another response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
